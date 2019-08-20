23ff050580a503eb50c324d65960b141202eb107
ijs <js230023@gmail.com>
2019-08-20 19:42:52 +0900

    프로젝트 생성 및 기본 세팅 완료

---
+++ src/main/java/com/example/demo/Board_1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Board_1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Board_1Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+	@RequestMapping("/")
+	@ResponseBody
+	public String main() {
+		return "메인화면";
+	}
+}
